d96381a4148fd07d16d4bde02f3fe5a20a19152b
Edis Gonuler <edis@gonuler.com>
2021-10-30 21:53:21 -0400

    add main styles and affiliates section

---
+++ components/AffiliatesSlider.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="affiliates">
+    <img src="images/cuny.png" alt="CUNY"/>
+    <img src="images/harvard.png" alt="Harvard"/>
+    <img src="images/fit.png" alt="FIT" />
+    <img src="images/photography.png" alt="Photography" />
+    <img src="images/pfp.png" alt="PFP" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AffiliatesSlider'
+}
+</script>
+<style lang="scss">
+
+.affiliates {
+  display: flex;
+  flex-direction: row;
+  overflow: auto;
+
+  &::-webkit-scrollbar {
+   display: none;
+  }
+
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+}
+
+.affiliates > img {
+  margin: 2rem;
+  max-height: 200px;
+}
+
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,54 @@
 <template>
-  <Tutorial/>
+  <div>
+    <div class="section">
+      <div class="section-heading">
+        <h1>Our Departments.</h1>
+        <p>How Civiso is getting its message across</p>
+      </div>
+    </div>
+
+    <div class="section">
+      <div class="section-heading">
+        <h1>Our Partners & Affiliates.</h1>
+        <p>The team at Civiso has the active support of faculty from</p>
+      </div>
+      <AffiliatesSlider />
+    </div>
+
+    <div class="section">
+      <div class="section-heading">
+        <h1>Civiso: <span class="highlight">Inspired.</span></h1>
+        <p>Calls to action written by our very own members</p>
+      </div>
+    </div>
+
+    <div class="section">
+      <div class="section-heading">
+        <h1>Civiso:  <span class="highlight">Toks.</span></h1>
+        <p>Raising awareness in 60 seconds or less.</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+import AffiliatesSlider from "../components/AffiliatesSlider";
+export default {
+  components: {AffiliatesSlider}
+}
 </script>
+
+<style lang="scss">
+ .section {
+   margin: 6rem 0;
+ }
+
+.section-heading {
+  h1 {
+    margin-bottom: 0.75rem;
+  }
+
+  margin-bottom: 2rem;
+}
+
+</style>
