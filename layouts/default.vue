9fef93fbe58d3f2270860b3529bdfca351f90f4f
Edis Gonuler <edis@gonuler.com>
2021-10-30 21:53:21 -0400

    add main styles + affiliates section + sticky navbar

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Navbar />
+
+    <div class="container">
+      <Nuxt />
+    </div>
+  </div>
+</template>
+<script>
+import Navbar from "../components/Navbar";
+export default {
+  components: {Navbar}
+}
+</script>
